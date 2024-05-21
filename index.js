067101cd98e03beaf04554247ecfbdeb4f3ac5b5
unknown <sohanpk24@gmail.com>
2024-05-22 04:15:49 +0600

    carts sent to the server site

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
 
     const menuCollection = client.db('bistroDb').collection('menu')
     const reviewCollection = client.db('bistroDb').collection('reviews')
+    const cartCollection = client.db('bistroDb').collection('carts')
 
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray();
@@ -42,6 +43,19 @@ async function run() {
         res.send(result)
     })
 
+
+    //cart collections
+    app.get('/carts', async(req, res)=>{
+      const result = await cartCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    app.post('/carts', async(req, res)=>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+    })
 
 
 
